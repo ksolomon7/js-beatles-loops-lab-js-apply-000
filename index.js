e02344e7dfa84e5541d99a3b116c0cea4631e85f
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-05 03:56:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function theBeatlesPlay(musicians, instruments){
+  let newString=[];
+  for(let i=0; i<musicians.length;i++){
+    let string= `${musicians[i] +  instruments[i]}`;
+    newString.push(string)
+  };
+  return newString;
+}
 // add solution here
